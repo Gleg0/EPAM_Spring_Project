a9b10ff16b6f3160df9da158adc35f03f91a41be
Гліб Кізін <glego200323@gmail.com>
2022-05-25 23:49:18 +0300

    Commit 2

--- src/main/java/com/epam/conference/controler/MainPageController.java
+++ src/main/java/com/epam/conference/controler/MainPageController.java
@@ -1,24 +1,15 @@
 package com.epam.conference.controler;
 
-import org.springframework.boot.web.servlet.server.Session;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
+import javax.servlet.http.HttpSession;
 import java.util.Locale;
 
 @Controller
 public class MainPageController{
     @GetMapping("/")
-    String mainGet(Model model, Session session){
+    String mainGet(Model model, HttpSession session,Locale locale){
         return "mainPage";
     }
 }
