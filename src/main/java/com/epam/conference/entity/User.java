3416018ce002b184509a975bd18262cd5b06c5be
Гліб Кізін <glego200323@gmail.com>
2022-05-28 17:20:11 +0300

    fix auto parameter generation

--- src/main/java/com/epam/conference/entity/User.java
+++ src/main/java/com/epam/conference/entity/User.java
@@ -1,6 +1,9 @@
 package com.epam.conference.entity;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Transient;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.hibernate.Hibernate;
