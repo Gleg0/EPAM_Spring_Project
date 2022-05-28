d7ac82336011926c63bddc9f3033dc89f9020fed
Fed333 <kovalchuk.roman03@gmail.com>
2022-05-28 19:40:48 +0300

    added login user demo

--- src/main/java/com/epam/conference/controler/MainPageController.java
+++ src/main/java/com/epam/conference/controler/MainPageController.java
@@ -1,15 +1,25 @@
 package com.epam.conference.controler;
 
+import com.epam.conference.entity.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import javax.servlet.http.HttpSession;
 import java.util.Locale;
+import java.util.Optional;
 
 @Controller
 public class MainPageController{
     @GetMapping("/")
-    String mainGet(Model model, HttpSession session,Locale locale){
+    public String mainGet(
+            Model model,
+            @AuthenticationPrincipal
+                    User user,
+            HttpSession session,
+            Locale locale
+    ){
+        Optional.ofNullable(user).ifPresent(u->model.addAttribute("user", user));
         return "mainPage";
     }
 }
