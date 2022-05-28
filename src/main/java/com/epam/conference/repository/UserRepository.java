8035c70060b9b99b5c4763a85b45625015888f03
Fed333 <kovalchuk.roman03@gmail.com>
2022-05-28 20:59:46 +0300

    removed redundant methods

--- src/main/java/com/epam/conference/repository/UserRepository.java
+++ src/main/java/com/epam/conference/repository/UserRepository.java
@@ -2,12 +2,10 @@ package com.epam.conference.repository;
 
 import com.epam.conference.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    User findByPasswordConfirm(String passwordConfirm);
-    @Query(value = "SELECT nextval(pg_get_serial_sequence('t_user', 'id'))", nativeQuery = true)
-    Long getNextId();
 
     User findByUsername(String username);
 }
