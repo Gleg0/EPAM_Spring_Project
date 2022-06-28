af45f26e124908231001d758f8ee46c16a5c2f97
Гліб Кізін <glego200323@gmail.com>
2022-06-28 19:31:32 +0300

    commit

--- src/main/java/com/epam/conference/controler/AcceptRequestPageController.java
+++ src/main/java/com/epam/conference/controler/AcceptRequestPageController.java
@@ -2,13 +2,9 @@ package com.epam.conference.controler;
 
 import com.epam.conference.service.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AcceptRequestPageController {
--- src/main/java/com/epam/conference/controler/LoginPageController.java
+++ src/main/java/com/epam/conference/controler/LoginPageController.java
@@ -1,14 +1,7 @@
 package com.epam.conference.controler;
 
-import com.epam.conference.entity.user.User;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import javax.servlet.http.HttpSession;
-import java.util.Locale;
-import java.util.Optional;
 
 @Controller
 public class LoginPageController {
